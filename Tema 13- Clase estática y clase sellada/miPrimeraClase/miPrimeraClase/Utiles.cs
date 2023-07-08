342a33eda09a95630e826b5734d68f961560c986
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-08 18:26:57 -0300

    Add files via upload

---
+++ Tema 13- Clase estática y clase sellada/miPrimeraClase/miPrimeraClase/Utiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimeraClase
+{
+    static class Utiles
+    {
+        static int numeroX = 2;
+        public static bool validarLongitud(int numero)
+        {
+            if (numero < numeroX)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
